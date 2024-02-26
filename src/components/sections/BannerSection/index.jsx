0c9e179802c23b9bc1b51521972631c019140ece
Ju Martins <jumartins958@hotmail.com>
2024-02-26 17:17:47 -0300

    Alterações

--- src/components/sections/BannerSection/index.jsx
+++ src/components/sections/BannerSection/index.jsx
@@ -10,7 +10,7 @@ export const BannerSection = () => {
           <p className='label'>{username}</p>
           <h1 className='title h1'>Bem vindo&#40;a&#41; ao meu portfólio</h1>
           <p className='paragraph'>Desenvolvedora Front-End</p>
-          <a href="../../../assets/curriculo-julia.pdf" download='curriculo-julia.pdf' type='application/pdf'><button type='button' className='btn'>Baixar Currículo</button></a>
+          <a href="/#contact"><button type='button' className='btn'>Contato</button></a>
         </div>
         <img className={styles.img} src={banner} alt='Imagem de computador' />
       </div>
